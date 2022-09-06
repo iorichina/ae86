26fadda5225b72baac0eaafe450813e1aa0c09eb
黄桂志 <iorihuang@kugou.net>
2022-09-06 19:24:49 +0800

    给出电压计算公式

--- voltage/voltage.ino
+++ voltage/voltage.ino
@@ -1,10 +1,12 @@
-// #include <Wire.h>
-
-int val11;
-int val2;
-
-const int analogInPin = A0;//A7;        // 模拟输入引脚 
-const int LED_PIN = 13;//2;//LED_BUILTIN; // led连接到pwm输出引脚 2
+#if defined(_STDINT_H)
+// esp32
+const int analogInPin = A7; // 35;        // 模拟输入引脚
+const int LED_PIN = 2;      // LED_BUILTIN; // led连接到pwm输出引脚
+#else
+// arduino
+const int analogInPin = A0; //       // 模拟输入引脚
+const int LED_PIN = 13;     // led连接到pwm输出引脚
+#endif
 
 int sensorValue = 0; // 从引脚读到的值
 int outputValue = 0; //输出到pwm脚的值
@@ -14,10 +16,9 @@ void setup()
   // 设置引脚为模拟输入模式
   // pinMode(analogInPin, INPUT);
   // 设置led脚输出pwm模式
-  // pinMode(LED_PIN, OUTPUT);
-
-  pinMode(LED_PIN, OUTPUT);    //配置GPIO2端口模式为输出模式
-  digitalWrite(LED_PIN, HIGH); //配置GPIO2端口为高电平，灯亮
+  pinMode(LED_PIN, OUTPUT);
+  //高电平，灯亮
+  digitalWrite(LED_PIN, HIGH);
 
   Serial.begin(115200);
   Serial.println("voltage detection start");
@@ -25,28 +26,40 @@ void setup()
 
 void loop()
 {
-  Serial.println("--------");
   //读取模拟输入数值
   sensorValue = analogRead(analogInPin);
+  //maybe more stable when the analog pin is read twice
+  sensorValue = analogRead(analogInPin);
   // 使用map函数把输入的数值进行映射
-  // outputValue = map(sensorValue, 0, 4095, 0, 330);//可以修改数值映射330   3.3V
-  // 改变模拟输出数值
-  // analogWrite(LED_PIN, outputValue);
-  // Serial.println((float)outputValue / 100.00); //保留两位小数
+#if defined(_STDINT_H)
+  Serial.println("----ESP32----");
+  // esp32 模拟输入电压5v，检测输入电压最大25v（被缩小了5倍）
+  outputValue = map(sensorValue, 0, 4095, 0, 499); //可以修改数值映射500   5V
   // 在串口打印显示输入输出的数值
   Serial.print("sensor = ");
   Serial.print(sensorValue);
-  // Serial.print("\t output = ");
-  // Serial.println(outputValue);
-
-  // float temp;
-  // val11 = analogRead(analogInPin); // ADC7==GPIO35==NODEMCU-32.P35
-  // Serial.println(val11);
-  // temp = val11 / 4.092;
-  // val11 = (int)temp;
-  // val2 = ((val11 % 100) / 10);
-  // Serial.println(val2);
-  delay(5000);
+  Serial.print("\t mapper = ");
+  Serial.print(outputValue);
+  Serial.print("\t voltage = ");
+  outputValue = (int)((sensorValue + 1) * (5.00 * 5) * 100 / 4096);
+  Serial.print(outputValue % 1000 / 100.0);
+  Serial.print("\t voltage = ");
+  outputValue = (int)((sensorValue + 1) * (4.72 * 5) * 100 / 4096);
+  Serial.println(outputValue % 1000 / 100.0);
+#else
+  // arduino
+  Serial.println("----Arduino----");
+  outputValue = map(sensorValue, 0, 1023, 0, 499); //可以修改数值映射330   3.3V
+  // 在串口打印显示输入输出的数值
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\t mapper = ");
+  Serial.print(outputValue);
+  Serial.print("\t voltage = ");
+  outputValue = (int)((sensorValue + 1) * (5.00 * 5) * 100 / 1024);
+  Serial.println(outputValue % 1000 / 100.0);
+#endif
 
   digitalWrite(LED_PIN, digitalRead(LED_PIN) ^ HIGH); //配置GPIO2端口为高电平，灯亮
+  delay(1000);
 }
